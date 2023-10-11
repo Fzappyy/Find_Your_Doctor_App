6466990e3dded8eef975809492d03ecc4345a18a
Felix Cena Jr. <felixcena15@gmail.com>
2023-10-11 21:24:03 +0800

    commit

---
+++ doctor_app/lib/components/home_screen_navbar.dart
@@ -0,0 +1,40 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+import 'package:doctor_app/constants.dart';
+import 'package:flutter_svg_provider/flutter_svg_provider.dart';
+
+class HomeScreenNavbar extends StatelessWidget {
+  const HomeScreenNavbar({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Container(
+          height: 24,
+          width: 24,
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: Svg(
+                'assets/svg/icon-burger.svg',
+                size: Size(
+                  24,
+                  24,
+                ),
+              ),
+            ),
+          ),
+        ),
+        Container(
+          width: 36,
+          height: 36,
+          child: const CircleAvatar(
+              backgroundColor: kBlueColor,
+              backgroundImage: AssetImage('assets/images/img-men-01.png')),
+        )
+      ],
+    );
+  }
+}
